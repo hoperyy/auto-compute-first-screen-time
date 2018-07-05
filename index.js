fae1a6de91fdd56501de3229b6771fc96fe56525
hoperyy <antfootmail@gmail.com>
2018-07-05 17:08:34 +0800

    [publish] 同时支持手动上报和自动上报

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ var supportPerformance = ('performance' in window) && ('getEntriesByType' in win
 
 // 轻量算法
 if (supportPerformance) {
-    module.exports = require('./prefer');
+    module.exports = require('./prefer').auto;
+    module.exports.report = require('./prefer').hand;
 } else if (window.performance && window.performance.timing) {
     // 较重的算法，通过不断的打点获取首屏
-    module.exports = require('./second');
+    module.exports = require('./second').auto;
+    module.exports.report = require('./second').hand;
 } else {
     // 如果连 performance.timing 都不支持，就不再上报了
     module.exports = function () { };
