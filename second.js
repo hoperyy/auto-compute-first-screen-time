ad2c50d8b41c31f072e9c5b695efa6a9cde14a3d
hoperyy <liuyuanyang@weidian.com>
2018-07-07 23:46:29 +0800

    代码优化，提高复用性

--- second.js
+++ second.js
@@ -5,108 +5,34 @@ var scriptStartTime = new Date().getTime();
 
 var win = window;
 var doc = win.document;
-var NAV_START_TIME = window.performance.timing.navigationStart;
 var util = require('./util');
 
 function generateApi() {
-    var global = initGlobal();
+    var global = util.mergeGlobal(util.initGlobal(), {
+        intervalDotTimer: null,
 
-    function initGlobal() {
-        return {
-            intervalDotTimer: null,
+        // dom 变化监听器
+        mutationObserver: null,
 
-            // dom 变化监听器
-            mutationObserver: null,
+        // 是否已经停止监听的标志
+        hasStoppedObserve: false,
 
-            // 是否已经停止监听的标志
-            hasStoppedObserve: false,
+        // 记录 Mutation 回调时的 dom 信息
+        dotList: [],
 
-            hasReported: false,
+        // 记录图片加载完成的时刻（唯一）
+        imgMap: {},
 
-            // 是否抓取过请求的标志位
-            isFirstRequestSent: false,
+        // 用于记录两次 global.mutationObserver 回调触发的时间间隔
+        lastDomUpdateTime: scriptStartTime,
 
-            // 设备信息，用于样本分析
-            device: {},
+        options: {
+            // 打点间隔
+            dotDelay: 250,
 
-            // 统计没有被计入首屏的图片有哪些，和更详细的信息
-            ignoredImages: [],
-
-            // 记录 Mutation 回调时的 dom 信息
-            dotList: [],
-
-            // 可以抓取请求的时间窗口队列
-            catchRequestTimeSections: [],
-
-            delayAll: 0,
-
-            // 记录图片加载完成的时刻（唯一）
-            imgMap: {},
-
-            // 用于记录两次 global.mutationObserver 回调触发的时间间隔
-            lastDomUpdateTime: scriptStartTime,
-
-            requestDetails: {},
-
-            // 一些可配置项，下面是默认值
-            options: {
-                onTimeFound: function () { },
-                request: {
-                    limitedIn: [],
-                    exclude: [/(sockjs)|(socketjs)|(socket\.io)/]
-                },
-
-                // 获取数据后，认为渲染 dom 的时长；同时也是串联请求的等待间隔
-                renderTimeAfterGettingData: 500,
-
-                // 找到首屏时间后，延迟上报的时间，默认为 500ms，防止页面出现需要跳转到登录导致性能数据错误的问题
-                delayReport: 500,
-
-                // onload 之后延时一段时间，如果到期后仍然没有异步请求发出，则认为是纯静态页面
-                watingTimeWhenDefineStaticPage: 1000,
-
-                // 打点间隔
-                dotDelay: 250,
-
-                abortTimeWhenDelay: 2000 // 监控打点会引起页面重绘，如果引发页面重绘的时间超过了该值，则不再做性能统计
-            }
-        };
-    }
-
-    function _getTime() {
-        return new Date().getTime();
-    }
-
-    function _parseUrl(url) {
-        var anchor = document.createElement('a');
-        anchor.href = url;
-        return anchor;
-    }
-
-    function _transRequestDetails2Arr() {
-        var requests = [];
-        var requestItem = {};
-
-        // 规范化 requests
-        for (var requestDetailKey in global.requestDetails) {
-            var parsedRequestDetailKey = requestDetailKey
-                .split(">time")[0]
-                .replace(/^http(s)?:/, '')
-                .replace(/^\/\//, '');
-
-            requestItem = {
-                src: parsedRequestDetailKey
-            };
-
-            for (var requestItemkey in global.requestDetails[requestDetailKey]) {
-                requestItem[requestItemkey] = global.requestDetails[requestDetailKey][requestItemkey];
-            }
-
-            requests.push(requestItem);
+            abortTimeWhenDelay: 2000 // 监控打点会引起页面重绘，如果引发页面重绘的时间超过了该值，则不再做性能统计
         }
-
-        return requests;
-    }
+    });
 
     function _getTargetDotObj() {
         // 倒序
@@ -146,7 +72,7 @@ function generateApi() {
             // 如果最终状态没有图片，则取出当前打点的对象，首屏时间设置为 performance 值
             targetInfo = global.dotList[0];
             targetInfo.firstScreenTimeStamp = performance.timing.domComplete; // 有 bug，过早获取时该值可能为 0
-            targetInfo.firstScreenTime = performance.timing.domComplete - NAV_START_TIME;
+            targetInfo.firstScreenTime = performance.timing.domComplete - util.NAV_START_TIME;
         }
 
         if (targetInfo) {
@@ -227,8 +153,8 @@ function generateApi() {
             dotList: global.dotList,
 
             delayAll: global.delayAll,
-            requests: _transRequestDetails2Arr(),
-            firstScreenTime: targetObj.firstScreenTimeStamp - NAV_START_TIME,
+            requests: util.transRequestDetails2Arr(global),
+            firstScreenTime: targetObj.firstScreenTimeStamp - util.NAV_START_TIME,
             firstScreenTimeStamp: targetObj.firstScreenTimeStamp,
             firstScreenImages: targetObj.firstScreenImages,
             firstScreenImagesLength: targetObj.firstScreenImages.length,
@@ -246,11 +172,11 @@ function generateApi() {
             return;
         }
 
-        var recordStartTime = _getTime();
+        var recordStartTime = util.getTime();
 
         var firstScreenImages = _getImages({ searchInFirstScreen: recordFirstScreen });
 
-        var recordEndTime = _getTime();
+        var recordEndTime = util.getTime();
 
         global.delayAll += recordEndTime - recordStartTime;
 
@@ -261,7 +187,7 @@ function generateApi() {
 
             firstScreenImages: firstScreenImages,
             firstScreenImagesLength: firstScreenImages.length,
-            blankTime: _getTime() - global.lastDomUpdateTime || _getTime(), // 距离上次记录有多久（用于调试）
+            blankTime: util.getTime() - global.lastDomUpdateTime || util.getTime(), // 距离上次记录有多久（用于调试）
             firstScreenTimeStamp: -1, // 当前时刻下，所有图片加载完毕的时刻
             timeStamp: recordStartTime, // 当前时刻
             duration: recordEndTime - recordStartTime,
@@ -290,10 +216,10 @@ function generateApi() {
             };
 
             var generateGlobalImgMapResult = function () {
-                var time = _getTime();
+                var time = util.getTime();
                 return {
                     onloadTimeStamp: time,
-                    onloadTime: time - NAV_START_TIME,
+                    onloadTime: time - util.NAV_START_TIME,
                 }
             };
 
@@ -370,7 +296,7 @@ function generateApi() {
         var imgList = [];
 
         var onImgSrcFound = function(imgSrc) {
-            var protocol = _parseUrl(imgSrc).protocol;
+            var protocol = util.parseUrl(imgSrc).protocol;
             if (protocol && protocol.indexOf('http') === 0) {
                 // 去重
                 if (imgList.indexOf(src) === -1) {
@@ -447,7 +373,7 @@ function generateApi() {
                 firstScreenTime: -1,
                 firstScreenTimeStamp: -1,
                 maxErrorTime: -1,
-                requests: _transRequestDetails2Arr(),
+                requests: util.transRequestDetails2Arr(global),
                 dotList: global.dotList,
                 firstScreenImages: _getImages({ searchInFirstScreen: true }),
                 delayAll: global.delayAll,
@@ -480,17 +406,7 @@ function generateApi() {
 
     // 插入脚本，用于获取脚本运行完成时间，这个时间用于获取当前页面是否有异步请求发出
     function insertTestTimeScript() {
-        window.addEventListener('load', function () {
-            // 如果脚本运行完毕，延时一段时间后，再判断页面是否发出异步请求，如果页面还没有发出异步请求，则认为该时刻为稳定时刻，尝试上报
-            var timer = setTimeout(function () {
-                // clear
-                clearTimeout(timer);
-
-                if (!global.isFirstRequestSent) {
-                    onStopObserving();
-                }
-            }, global.options.watingTimeWhenDefineStaticPage);
-        });
+        util.insertTestTimeScript(onStopObserving, global);
     }
 
     // 监听 dom 变化，脚本运行时就开始
@@ -498,7 +414,7 @@ function generateApi() {
         var lastObserverRunTime;
 
         var dotCallback = function (param) {
-            var now = _getTime();
+            var now = util.getTime();
             if (lastObserverRunTime && now - lastObserverRunTime < global.options.dotDelay) {
                 return;
             }
@@ -528,229 +444,11 @@ function generateApi() {
     }
 
     function overrideRequest() {
-        var requestTimerStatusPool = {};
-
-        var isRequestDetailsEmpty = function () {
-            for (var key in global.requestDetails) {
-                if (global.requestDetails[key] && global.requestDetails[key].status !== 'complete') {
-                    return false;
-                }
-            }
-
-            return true;
-        };
-
-        var isRequestTimerPoolEmpty = function () {
-            for (var key in requestTimerStatusPool) {
-                if (requestTimerStatusPool[key] !== 'stopped') {
-                    return false;
-                }
-            }
-
-            return true;
-        };
-
-        var shouldCatchThisRequest = function (url) {
-            // 默认抓取该请求到队列，认为其可能影响首屏
-            var shouldCatch = true;
-
-            // 如果已经上报，则不再抓取请求
-            if (global.hasStoppedObserve) {
-                shouldCatch = false;
-            }
-
-            var sendTime = _getTime();
-
-            // 如果发送数据请求的时间点在时间窗口内，则认为该抓取该请求到队列，主要抓取串联型请求
-            for (var sectionIndex = 0; sectionIndex < global.catchRequestTimeSections.length; sectionIndex++) {
-                var poolItem = global.catchRequestTimeSections[sectionIndex];
-                if (sendTime >= poolItem[0] && sendTime <= poolItem[1]) {
-                    break;
-                }
-            }
-            if (global.catchRequestTimeSections.length && sectionIndex === global.catchRequestTimeSections.length) {
-                shouldCatch = false;
-            }
-
-            // 如果发送请求地址不符合白名单和黑名单规则。则认为不该抓取该请求到队列
-            for (var i = 0, len = global.options.request.limitedIn.length; i < len; i++) {
-                if (!global.options.request.limitedIn[i].test(url)) {
-                    shouldCatch = false;
-                }
-            }
-
-            for (var i = 0, len = global.options.request.exclude.length; i < len; i++) {
-                if (global.options.request.exclude[i].test(url)) {
-                    shouldCatch = false;
-                }
-            }
-
-            return shouldCatch;
-        }
-
-        var ensureRequestDetail = function (requestKey) {
-            if (!global.requestDetails[requestKey]) {
-                global.requestDetails[requestKey] = {
-                    status: '',
-                    completeTimeStamp: '',
-                    completeTime: '',
-                    type: ''
-                };
-            }
-        };
-
-        var onRequestSend = function (url, type) {
-            if (!global.isFirstRequestSent) {
-                global.isFirstRequestSent = true;
-            }
-
-            var requestKey = url + '>time:' + _getTime();
-            ensureRequestDetail(requestKey);
-
-            global.requestDetails[requestKey].status = 'sent';
-            global.requestDetails[requestKey].type = type;
-
-            requestTimerStatusPool[requestKey] = 'start';
-
-            return {
-                requestKey: requestKey
-            }
-        };
-
-        var afterRequestReturn = function (requestKey) {
-            //  当前时刻
-            var returnTime = _getTime();
-
-            ensureRequestDetail(requestKey);
-
-            // 标记这个请求完成
-            global.requestDetails[requestKey].status = 'complete';
-            global.requestDetails[requestKey].completeTimeStamp = returnTime;
-            global.requestDetails[requestKey].completeTime = returnTime - NAV_START_TIME;
-
-
-            // 从这个请求返回的时刻起，在较短的时间段内的请求也需要被监听
-            global.catchRequestTimeSections.push([returnTime, returnTime + global.options.renderTimeAfterGettingData]);
-
-            var timer = setTimeout(function () {
-                requestTimerStatusPool[requestKey] = 'stopped';
-                if (isRequestDetailsEmpty() && isRequestTimerPoolEmpty()) {
-                    onStopObserving();
-                }
-                clearTimeout(timer);
-            }, global.options.renderTimeAfterGettingData);
-        };
-
-        var overideXhr = function (onRequestSend, afterRequestReturn) {
-            var XhrProto = XMLHttpRequest.prototype;
-            var oldXhrSend = XhrProto.send;
-            XhrProto.send = function () {
-                if (shouldCatchThisRequest(this._http.url)) {
-                    var requestKey = onRequestSend(this._http.url, 'xhr').requestKey;
-
-                    var oldReadyCallback = this.onreadystatechange;
-                    this.onreadystatechange = function () {
-                        if (this.readyState === 4) {
-                            afterRequestReturn(requestKey);
-                        }
-
-                        if (oldReadyCallback && oldReadyCallback.apply) {
-                            oldReadyCallback.apply(this, arguments);
-                        }
-                    };
-                }
-
-                return oldXhrSend.apply(this, [].slice.call(arguments));
-            };
-        };
-
-        var overrideFetch = function (onRequestSend, afterRequestReturn) {
-            if (window.fetch && typeof Promise === 'function') {
-                // ensure Promise exists. If not, skip cathing request
-                var oldFetch = window.fetch;
-                window.fetch = function () {
-                    var _this = this;
-                    var args = arguments;
-
-                    return new Promise(function (resolve, reject) {
-                        var url;
-                        var requestKey;
-
-                        if (typeof args[0] === 'string') {
-                            url = args[0];
-                        } else if (typeof args[0] === 'object') { // Request Object
-                            url = args[0].url;
-                        }
-
-                        // when failed to get fetch url, skip report
-                        if (url) {
-                            // console.warn('[auto-compute-first-screen-time] no url param found in "fetch(...)"');
-                            requestKey = onRequestSend(url, 'fetch').requestKey;
-                        }
-
-                        oldFetch.apply(_this, args).then(function (response) {
-                            if (requestKey) {
-                                afterRequestReturn(requestKey);
-                            }
-                            resolve(response);
-                        }).catch(function (err) {
-                            if (requestKey) {
-                                afterRequestReturn(requestKey);
-                            }
-                            reject(err);
-                        });
-                    })
-                };
-            }
-        };
-
-        // overide fetch first, then xhr, because fetch could be mocked by xhr
-        overrideFetch(onRequestSend, afterRequestReturn);
-
-        overideXhr(onRequestSend, afterRequestReturn);
+        util.overrideRequest(global, onStopObserving);
     }
 
     function mergeUserOptions(userOptions) {
-        if (userOptions) {
-            if (userOptions.delayReport) {
-                global.options.delayReport = userOptions.delayReport;
-            }
-
-            if (userOptions.watingTimeWhenDefineStaticPage) {
-                global.options.watingTimeWhenDefineStaticPage = userOptions.watingTimeWhenDefineStaticPage;
-            }
-
-            if (userOptions.onTimeFound) {
-                global.options.onTimeFound = function () {
-                    var _this = this;
-                    var args = arguments;
-
-                    // delay a piece of time for reporting
-                    var timer = setTimeout(function () {
-                        userOptions.onTimeFound.apply(_this, args);
-                        clearTimeout(timer);
-                    }, global.options.delayReport);
-                };
-            }
-
-            var requestConfig = userOptions.request || userOptions.xhr;
-            if (requestConfig) {
-                if (requestConfig.limitedIn) {
-                    global.options.request.limitedIn = global.options.request.limitedIn.concat(requestConfig.limitedIn);
-                }
-                if (requestConfig.exclude) {
-                    global.options.request.exclude = global.options.request.exclude.concat(requestConfig.exclude);
-                }
-            }
-
-            if (userOptions.renderTimeAfterGettingData) {
-                global.options.renderTimeAfterGettingData = userOptions.renderTimeAfterGettingData;
-            }
-
-            if (userOptions.onAllXhrResolved) {
-                global.options.onAllXhrResolved = userOptions.onAllXhrResolved;
-            }
-        }
+        util.mergeUserOptions(global, userOptions);
     }
 
     return {
