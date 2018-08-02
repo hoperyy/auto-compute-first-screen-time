4afa2dacd82d0f5e825c7adea069b821ea846a3f
hoperyy <antfootmail@gmail.com>
2018-08-02 23:43:00 +0800

    [feat] add config of tags for changing navigation start time

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
 
 module.exports = {
-    version: '5.0.1',
+    version: '5.0.2',
 
     NAV_START_TIME: window.performance.timing.navigationStart,
 
